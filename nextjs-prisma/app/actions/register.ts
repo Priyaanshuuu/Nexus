f069103da9c3b1ec0c6bfdb6f49c4a3d24a9a58e
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2026-01-07 18:07:51 +0530

    authentication_code

---
+++ nextjs-prisma/app/actions/register.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import {prisma}  from "@/app/lib/prisma";
+import bcrypt from "bcryptjs";
+
+export const register = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const name = formData.get("name") as string;
+
+  if (!email || !password || !name) {
+    return { error: "Missing fields" };
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created!" };
+};
---
+++ nextjs-prisma/app/lib/auth.ts
@@ -0,0 +1,59 @@
+
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import {prisma}  from "./prisma";
+import bcrypt from "bcryptjs"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+
+      GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID!,
+        clientSecret : process.env.GOOGLE_SECRET!
+
+    }),
+
+
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!user || !user.password) {
+          return null; 
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+})
